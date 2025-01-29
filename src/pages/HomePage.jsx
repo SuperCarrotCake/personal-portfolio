39c79146fa46af876344f05c503be1af64317d2b
SuperCarrotCake <nicolsrojas.contacto@gmail.com>
2025-01-29 12:10:02 -0500

    Added routing, and dynamic base for github

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+import Hero from "../components/Hero";
+
+function HomePage() {
+    return(
+      <>
+        <Header/>
+        <Hero/>
+        <Footer/>
+      </>
+    );
+}
+
+export default HomePage;
